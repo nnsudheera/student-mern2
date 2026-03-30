7909cce72e94c4d50a4d5e199418a905519c3d60
nnsudheera <nnsudheera@gmail.com>
2026-03-30 11:26:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,11 @@ app.use(express.json());
 
 // ✅ Use ENV variable instead of local MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000
+  })
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("Mongo Error:", err));
 
 app.use("/api/students", require("./routes/studentRoutes"));
 
